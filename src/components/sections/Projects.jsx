431bf2a5691f8fd411a17cfeb40fd95c144ab8b3
Yousef-Taha1 <ytaha21@gmail.com>
2025-02-13 16:26:46 +0100

    Added a project, resume and fixed title + hamburger

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -49,6 +49,41 @@ export const Projects = () => {
                         </a>
                     </div>
                 </div>
+
+                <div 
+                className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
+                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                >
+                    <h3 className="text-xl font-bold mb-2">Distance Measurement</h3>
+                    <li className="text-gray-400 mb-3">
+                    Developed C programs for Arduino Nano to measure the ultrasound echo time and store the data.
+                    </li>
+                    <li className="text-gray-400 mb-3">
+                    Developed Python programs to retrieve the stored data, calculate the distance based on the time, and 
+                    plot the result using NumPy and Pyplot.
+                    </li>
+                    <div className="flex flex-wrap gap-2 mb-4">
+                        {["C", "Python", "Ultrasound sensor", "Arduino Nano"].map((tech,key) => (
+                            <span 
+                            key = {key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                            >
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
+                    {/* <div className="flex justify-between items-center">
+                        <a 
+                        href="https://www.youtube.com/watch?v=31pBwrbYGa8" 
+                        className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        > 
+                        View Project 🎥
+                        </a>
+                    </div> */}
+                </div>
             </div>
         </div>
         </RevealOnScroll>
