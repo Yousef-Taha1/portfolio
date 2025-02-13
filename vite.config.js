026af96f34dba63a29473973656929a2659fbf79
Yousef-Taha1 <ytaha21@gmail.com>
2025-02-13 14:09:09 +0100

    vite.config.js #3

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/',
-  output:"export",
+  base: '/portfolio',
 });
