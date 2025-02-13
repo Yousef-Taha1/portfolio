902e809c88fbdc9aed9f5f2d5ac0d8441fd49e93
Yousef-Taha1 <ytaha21@gmail.com>
2025-02-13 03:41:05 +0100

    first commit

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,105 @@
+import {RevealOnScroll} from '../RevealOnScroll'
+
+
+export const About = () => {
+
+    const frontendSkills = [
+        "React", 
+        "TypeScript",
+        "JavaScript",
+        "HTML",
+        "CSS", 
+        "TailwindCSS"
+    ];
+
+    const backendSkills = [
+        "C", 
+        "Java", 
+        "Python"
+    ];
+
+
+    return (
+        <section 
+        id="about" 
+        className="min-h-screen flex items-center justify-center py-20"
+        >
+            <RevealOnScroll>
+            <div className="max-w-3xl mx-auto px-4">
+                <h2 
+                    className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"
+                >   
+                About Me
+                </h2>
+
+                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                    <p className="text-gray-300 mb-6">
+                    Passionate and self-motivated student with hands-on experience in both software and hardware systems.
+                    Focused on developing scalable web applications and crafting innovative solutions to complex challenges.
+                    </p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {frontendSkills.map((tech, key) => (
+                                    <span 
+                                    key = {key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {backendSkills.map((tech, key) => (
+                                    <span 
+                                    key = {key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                        <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+                            <ul className="list-disc list-inside text-gray-300 space-y-2">
+                                <li>
+                                    <strong> B.S. in Information Engineering </strong> - Hamburg University of Applied Sciences (2022-Current)
+                                </li>
+                                <li>
+                                    Relevant Coursework: C, Java, Data Structures & Algorithms, Databases, Embedded Programming,
+                                    Internet of Things...
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+                            <div className="space-y-4 text-gray-300">
+                                <div>
+                                    <li className="font-semibold"> Store Manager at I Cook Arabs (Kuala Lumpur, Malaysia) (2021-2022)</li>
+                                    <p> Opening, closing, operating registers and point of sales.</p>
+                                    <p> Maintaining stock in-store and through sheets.</p>
+                                </div>
+                                <div>
+                                    <li className="font-semibold"> Manager at Horizons Net Cafe & IT Store (Damascus, Syria) (2017-2019)</li>
+                                    <p> Maintaining the stability of computers and managing customer accounts.</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            </RevealOnScroll>
+        </section>
+    )
+}
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,57 @@
+import {RevealOnScroll} from '../RevealOnScroll'
+
+
+export const Projects = () => {
+    return (
+        <section 
+            id="projects" 
+            className="min-h-screen flex items-center justify-center py-20"
+        >
+            <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+                 Featured Projects
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* First Project */}
+                <div 
+                className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
+                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                >
+                    <h3 className="text-xl font-bold mb-2">IoT-powered Pet Simulator</h3>
+                    <li className="text-gray-400 mb-3">
+                    Created a web application that synchronously retrieves and displays virtual pet data from a connected 
+                    physical device 
+                    </li>
+                    <li className="text-gray-400 mb-3">
+                    Contributed to the design and development of the application’s workflow as a frontend team member, 
+                    collaborating with four colleagues to ensure an agile workflow and efficient deployments
+                    </li>
+                    <div className="flex flex-wrap gap-2 mb-4">
+                        {["React", "Node.js", "Bootstrap", "Docker"].map((tech,key) => (
+                            <span 
+                            key = {key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                            >
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <a 
+                        href="https://www.youtube.com/watch?v=31pBwrbYGa8" 
+                        className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        > 
+                        View Project 🎥
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </RevealOnScroll>
+        </section>
+    )
+}
